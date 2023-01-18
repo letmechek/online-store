8d7b8e62d2d47e52b9dba20d48569ea98ef0957d
letmechek <abdullahi.ali1017@gmail.com>
2023-01-17 20:23:24 -0800

    updates navbar to use @mui

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,34 +1,127 @@
-import { Link } from 'react-router-dom'
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
 import * as userService from '../../utilities/users-service'
-import React, { useState } from "react"
-import './NavBar.css'
+import { Link } from 'react-router-dom'
+// import InboxIcon from '@mui/icons-material/MoveToInbox';
+// import MailIcon from '@mui/icons-material/Mail';
 
+export default function TemporaryDrawer({user, setUser}) {
+  const [state, setState] = React.useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false,
+  });
+  function handleLogOut() {
+            userService.logOut()
+            setUser(null)
+        }
 
-export default function NavBar({ user, setUser }) {
-    const [navbarOpen, setNavbarOpen] = useState(false)
-    function handleLogOut() {
-        userService.logOut()
-        setUser(null)
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
     }
-    const handleToggle = () => {
-        setNavbarOpen(!navbarOpen)
-      }
 
-    return (
-        <nav className="navBar" >
-        <button className="sandwichbtn" onClick={handleToggle}><i class="gg-format-justify"></i></button>
-        <ul className={`menuNav ${navbarOpen ? " showMenu" : ""}`}> <nav>
+    setState({ ...state, [anchor]: open });
+  };
+
+  const list = (anchor) => (
+    <Box
+      sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        
+        <ListItem disablePadding>
+        <ListItemButton>
         Welcome, {user.name}
-        <Link to="/">Home</Link>
+        </ListItemButton>
+        </ListItem>
+      </List>
+      <Divider />
+      <List>
+        {[
+          <Link to="/">Home</Link>, 
+          <Link to="/products/new">Catalogue</Link>, 
+          <Link to="" onClick={handleLogOut}>Log Out</Link>
+        ].map((item, index) => (
+          <ListItem key={index} disablePadding>
+            <ListItemButton>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      
+    </Box>
+  );
+
+  return (
+    <div>
+      {['left', 'right', 'top', 'bottom'].map((anchor) => (
+        <React.Fragment key={anchor}>
+          <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
+          <Drawer
+            anchor={anchor}
+            open={state[anchor]}
+            onClose={toggleDrawer(anchor, false)}
+          >
+            {list(anchor)}
+          </Drawer>
+        </React.Fragment>
+      ))}
+    </div>
+  );
+}
+
+
+
+
+
+
+
+
+// import { Link } from 'react-router-dom'
+// import * as userService from '../../utilities/users-service'
+// import React, { useState } from "react"
+// import './NavBar.css'
+
+
+// export default function NavBar({ user, setUser }) {
+//     const [navbarOpen, setNavbarOpen] = useState(false)
+//     function handleLogOut() {
+//         userService.logOut()
+//         setUser(null)
+//     }
+//     const handleToggle = () => {
+//         setNavbarOpen(!navbarOpen)
+//       }
+
+//     return (
+//         <nav className="navBar" >
+//         <button className="sandwichbtn" onClick={handleToggle}><i class="gg-format-justify"></i></button>
+//         <ul className={`menuNav ${navbarOpen ? " showMenu" : ""}`}> <nav>
+//         Welcome, {user.name}
+//         <Link to="/">Home</Link>
         
-        <Link to="/products/new">Catalogue</Link>
+//         <Link to="/products/new">Catalogue</Link>
         
-        {/* <Link to="/orders">Order History</Link> */}
+//         {/* <Link to="/orders">Order History</Link> */}
         
         
-        <Link to="" onClick={handleLogOut}>Log Out</Link>
-    </nav></ul>
-      </nav>
-        )
-    }
+//         <Link to="" onClick={handleLogOut}>Log Out</Link>
+//     </nav></ul>
+//       </nav>
+//         )
+//     }
    
--- src/pages/OrderHistoryPage/OrderHistoryPage.jsx
+++ src/pages/OrderHistoryPage/OrderHistoryPage.jsx
@@ -1,21 +1,56 @@
 import './OrderHistoryPage.css';
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
 import Logo from '../../components/Logo/Logo';
+
+const Input = styled.input`
+  width: 100%;
+  padding: 10px;
+  margin: 10px 0;
+  border: none;
+  border-bottom: 2px solid #ccc;
+  font-size: 18px;
+  outline: none;
+  &:focus {
+    border-bottom: 2px solid #4caf50;
+  }
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0% 30% 0% 30%;
+`;
+
+const Button = styled.button`
+  width: 100%;
+  padding: 10px;
+  margin: 10px 0;
+  background-color: #4caf50;
+  color: white;
+  font-size: 18px;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  &:hover {
+    background-color: #3e8e41;
+  }
+`;
 
 export default function OrderHistoryPage({ user, setUser }) {
   return (
     <>
-        {/* <UserLogOut user={user} setUser={setUser} /> */}
-   
-        {/* <Link to="/products/new" className="button btn-sm">NEW ORDER</Link> */}
-        <h1>Payment Information</h1>
-       <div className='payment'> <input id="column-left" type="text" name="first-name" placeholder="First Name"/>
-          <input id="column-right" type="text" name="last-name" placeholder="Surname"/>
-          <input id="input-field" type="text" name="number" placeholder="Card Number"/>
-          <input id="column-left" type="text" name="expiry" placeholder="MM / YY"/>
-          <input id="column-right" type="text" name="cvc" placeholder="CCV"/>
-          <input id="input-button" type="submit" value="Submit"/>
-          </div>
-          <Logo /> 
-        </>
+      <h1>Payment Information</h1>
+      <Form>
+        <Input type="text" placeholder="First Name" />
+        <Input type="text" placeholder="Surname" />
+        <Input type="text" placeholder="Card Number" />
+        <Input type="text" placeholder="MM / YY" />
+        <Input type="text" placeholder="CCV" />
+        <Button>Submit</Button>
+      </Form>
+      <Logo />
+    </>
   );
 }

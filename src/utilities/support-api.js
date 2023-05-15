c416eba45024ee05cea77944bd0f5b822c595a87
letmechek <abdullahi.ali1017@gmail.com>
2023-05-15 16:59:01 -0400

    updates api server

--- src/utilities/support-api.js
+++ src/utilities/support-api.js
@@ -1,27 +1,15 @@
-// import axios from "axios";
-
-// const BASE_URL = "/api/support";
-// export async function sendMessage(message) {
-//   try {
-//     const response = await axios.post(`${BASE_URL}/message`, {
-//       message: message,
-//     });
-//     return response;
-//   } catch (error) {
-//     throw error;
-//   }
-// }
-
-import { sendRequest } from './send-request';
+import axios from "axios";
 
 const BASE_URL = "/api/support";
-
 export async function sendMessage(message) {
   try {
-    const response = await sendRequest(`${BASE_URL}/message`, 'POST', { message });
+    const response = await axios.post(`${BASE_URL}/message`, {
+      message: message,
+    });
     return response;
   } catch (error) {
     throw error;
   }
 }
+
 

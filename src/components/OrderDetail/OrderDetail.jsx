57af69d49693733649926e0f9b823e8225c16bee
letmechek <abdullahi.ali1017@gmail.com>
2023-05-19 14:54:47 -0400

    adds cart icon

--- src/components/OrderDetail/OrderDetail.jsx
+++ src/components/OrderDetail/OrderDetail.jsx
@@ -1,64 +1,92 @@
-import './OrderDetail.css';
+import React, { useState } from 'react';
+import { IconButton, Badge, Box, Typography, Button } from '@mui/material';
+import { ShoppingCart } from '@mui/icons-material';
+
 import LineItem from '../LineItem/LineItem';
-import { useState } from 'react';
 
 export default function OrderDetail({ order, handleChangeQty, handleCheckout }) {
   const [navbarOpen, setNavbarOpen] = useState(false);
-  
+
   if (!order) return null;
 
-  const lineItems = order.lineItems.map(product =>
+  const lineItems = order.lineItems.map((product) => (
     <LineItem
       lineItem={product}
       isPaid={order.isPaid}
       key={product._id}
       handleChangeQty={handleChangeQty}
     />
-  );
-  const handleToggle = () => {
-    setNavbarOpen(!navbarOpen)
-  }
+  ));
 
-  return (<>
-    <nav className="navBar1" >
-        <button className="sandwichbtn1" onClick={handleToggle}><i class="gg-shopping-bag"></i></button>
-        <div className={`menuNav1 ${navbarOpen ? " showMenu1" : ""}`}> 
-        <nav>
-    <div className="OrderDetail">
-      <div className="section-heading">
-        {order.isPaid ?
-          <span>ORDER <span className="smaller">{order.orderId}</span></span>
-          :
-          <span>NEW ORDER</span>
-        }
-        <span>{new Date(order.updatedAt).toLocaleDateString()}</span>
-      </div>
-      <div className="line-item-container flex-ctr-ctr flex-col scroll-y">
-        {lineItems.length ?
-          <>
-            {lineItems}
-            <section className="total">
-            {order.isPaid ? (
-                        <span className="right">TOTAL&nbsp;&nbsp;</span>
+  const handleToggle = () => {
+    setNavbarOpen(!navbarOpen);
+  };
+
+  return (
+    <nav>
+      <Box sx={{ display: 'flex', alignItems: 'center' }}>
+        <IconButton aria-label="Cart" onClick={handleToggle}sx={{left:'95%',marginTop:'1%'}}>
+          <Badge badgeContent={order.totalQty} color="primary">
+            <ShoppingCart />
+          </Badge>
+        </IconButton>
+        {navbarOpen && (
+          <div className="menuNav1">
+            <div>
+              <div className="section-heading">
+                {order.isPaid ? (
+                  <>
+                    <Typography variant="body1">ORDER</Typography>
+                    <Typography variant="subtitle2" className="smaller">
+                      {order.orderId}
+                    </Typography>
+                  </>
+                ) : (
+                  <Typography variant="body1">NEW ORDER</Typography>
+                )}
+                <Typography variant="body2">
+                  {new Date(order.updatedAt).toLocaleDateString()}
+                </Typography>
+              </div>
+              <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', mt: 2 }}>
+                {lineItems.length ? (
+                  <>
+                    {lineItems}
+                    <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-end', mt: 2 }}>
+                      {order.isPaid ? (
+                        <Typography variant="body2" sx={{ textAlign: 'right' }}>
+                          TOTAL
+                        </Typography>
                       ) : (
                         <form action="/create-checkout-session" method="POST">
-                          <button type="submit" className="btn-checkout" disabled={!lineItems.length}>
+                          <Button
+                            type="submit"
+                            variant="contained"
+                            disabled={!lineItems.length}
+                            sx={{ backgroundColor: '#ff4081', color: 'white' }}
+                          >
                             CHECKOUT
-                          </button>
+                          </Button>
                         </form>
                       )}
-              <span>{order.totalQty}</span>
-              <span className="right">${order.orderTotal.toFixed(2)}</span>
-            </section>
-          </>
-          :
-          <div className="hungry">cart is empty?</div>
-        }
-      </div>
-    </div>
+                      <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '100%' }}>
+                        <Typography variant="body2">{order.totalQty}</Typography>
+                        <Typography variant="body2" sx={{ textAlign: 'right' }}>
+                          ${order.orderTotal.toFixed(2)}
+                        </Typography>
+                      </Box>
+                    </Box>
+                  </>
+                ) : (
+                  <Typography variant="body2" className="hungry">
+                    Cart is empty?
+                  </Typography>
+                )}
+              </Box>
+            </div>
+          </div>
+        )}
+      </Box>
     </nav>
-    </div>
-      </nav>
-      </>
   );
 }
